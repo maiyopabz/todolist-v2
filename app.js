22d54a22d22d30085fc6a40f593e75a4ac4f1682
maiyo10 <maiyopabz@gmail.com>
2021-12-29 02:01:47 -0800

    switch statement

--- app.js
+++ app.js
@@ -3,17 +3,47 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req, res ){
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
 
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-    if (today.getDay() === 2 || today.getDay() === 0) {
-        res.send("Yay its the weekend!");
-    } else {
-        res.send("BOO I HAVE WORK!");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Sunday";
+            break;
+        default:
+        console.log("error: current day is equal to:" + currentDay);
+
     }
+
+    res.render("list", {
+        kindOfDay: day
+    });
+
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
